5e31906040ebef3ebf1db279641330dbaa6ea0a4
Jeramy Harrison <dbralir@gmail.com>
2015-11-19 14:10:33 -0500

    Fixed query return type

--- src/test_query.cpp
+++ src/test_query.cpp
@@ -32,39 +32,39 @@ TEST_CASE("Query compiles", "[ginseng]")
     make_ent(true, true);
 
     {
-        std::vector<std::tuple<DB::EntID>> q = db.query<>();
+        std::vector<std::tuple<>> q = db.query<>();
         REQUIRE(q.size() == 10);
     }
     {
-        std::vector<std::tuple<DB::EntID,Data1&>> q = db.query<Data1>();
+        std::vector<std::tuple<Data1>> q = db.query<Data1>();
         REQUIRE(q.size() == 7);
     }
     {
-        std::vector<std::tuple<DB::EntID,Data2&>> q = db.query<Data2>();
+        std::vector<std::tuple<Data2&>> q = db.query<Data2&>();
         REQUIRE(q.size() == 6);
     }
     {
-        std::vector<std::tuple<DB::EntID,Data1&,Data2&>> q = db.query<Data1, Data2>();
+        std::vector<std::tuple<Data1&,Data2&>> q = db.query<Data1&, Data2&>();
         REQUIRE(q.size() == 4);
     }
     {
-        std::vector<std::tuple<DB::EntID,Not<Data1>>> q = db.query<Not<Data1>>();
+        std::vector<std::tuple<Not<Data1>>> q = db.query<Not<Data1>>();
         REQUIRE(q.size() == 3);
     }
     {
-        std::vector<std::tuple<DB::EntID,Not<Data2>>> q = db.query<Not<Data2>>();
+        std::vector<std::tuple<Not<Data2>>> q = db.query<Not<Data2>>();
         REQUIRE(q.size() == 4);
     }
     {
-        std::vector<std::tuple<DB::EntID,DB::ComInfo<Data1>,Not<Data2>>> q = db.query<DB::ComInfo<Data1>, Not<Data2>>();
+        std::vector<std::tuple<DB::ComInfo<Data1>,Not<Data2>>> q = db.query<DB::ComInfo<Data1>, Not<Data2>>();
         REQUIRE(q.size() == 3);
     }
     {
-        std::vector<std::tuple<DB::EntID,DB::ComInfo<Data2>,Not<Data1>>> q = db.query<DB::ComInfo<Data2>, Not<Data1>>();
+        std::vector<std::tuple<DB::ComInfo<Data2>,Not<Data1>>> q = db.query<DB::ComInfo<Data2>, Not<Data1>>();
         REQUIRE(q.size() == 2);
     }
     {
-        std::vector<std::tuple<DB::EntID,Not<Data1>,Not<Data2>>> q = db.query<Not<Data1>, Not<Data2>>();
+        std::vector<std::tuple<Not<Data1>,Not<Data2>>> q = db.query<Not<Data1>, Not<Data2>>();
         REQUIRE(q.size() == 1);
     }
 }
